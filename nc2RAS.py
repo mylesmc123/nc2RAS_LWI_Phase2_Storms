7b2670c808af6bea8b55d5b687aeb63184a03f13
Myles <mmcmanus@thewaterinstitute.org>
2022-07-19 15:19:50 -0500

    na

---
+++ nc2RAS.py
@@ -0,0 +1,4 @@
+# This script is meant to match the functionality of nc2RAS.m Matlab script
+# Converts an input netCDF file to be used  for the 500 different equiprobable rainfall fields derived from the IPET predicted rainfall for selected historic rainfall events
+
+# Import List of selected historic rainfall events and their respective startTimes
